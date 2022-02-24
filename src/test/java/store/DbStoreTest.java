24e5ed538f82fb0e656fb64375c5fd99ad37fe1d
yarmail <yarmail@yandex.ru>
2022-02-24 17:48:31 +0300

    3. Тесты на DbStore [#504817] add tests

--- src/test/java/store/DbStoreTest.java
+++ src/test/java/store/DbStoreTest.java
@@ -1,17 +1,60 @@
 package store;
 
+import model.Candidate;
 import model.Post;
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class DbStoreTest {
+    Store store = DbStore.instOf();
+    Post post = new Post(0, "Java Job");
+    Candidate candidate = new Candidate(0, "New candidate");
+
+    @Before
+    public void before() {
+        store.savePost(post);
+        store.saveCandidate(candidate);
+    }
+
     @Test
     public void whenCreatePost() {
-        Store store = DbStore.instOf();
-        Post post = new Post(0, "Java Job");
-        store.savePost(post);
         Post postInDb = store.findByIdPost(post.getId());
         assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenDeletePost() {
+        store.deletePost(post.getId());
+        Post postInDb = store.findByIdPost(post.getId());
+        assertNull(postInDb);
+    }
+
+    @Test
+    public void whenFindByIdPost() {
+        Post postInDb = store.findByIdPost(post.getId());
+        assertEquals(postInDb, post);
+    }
+
+    @Test
+    public void whenCreateCandidate() {
+        Candidate candidateInDb = store.findByIdCandidate(candidate.getId());
+        assertThat(candidateInDb.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenDeleteCandidate() {
+        store.deleteCandidate(candidate.getId());
+        Candidate candidateInDb = store.findByIdCandidate(candidate.getId());
+        assertNull(candidateInDb);
+    }
+
+    @Test
+    public void whenFindByIdCandidate() {
+        Candidate candidateInDb = store.findByIdCandidate(candidate.getId());
+        assertEquals(candidateInDb, candidate);
     }
 }
